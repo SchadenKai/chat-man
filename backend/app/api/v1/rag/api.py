abb289d0d1fe3d4404005c754d809d97f632cd4a
SchadenKai <kairutecsonpersonal@gmail.com>
2025-12-22 17:21:04 +0800

    Refactor code structure for improved readability and maintainability

--- backend/app/api/v1/rag/api.py
+++ backend/app/api/v1/rag/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, UploadFile
 from fastapi.responses import StreamingResponse
 
 from langchain_core.language_models import BaseChatModel
@@ -14,5 +14,11 @@ router = APIRouter(prefix="/rag")
 
 
 @router.post("/ingest")
-async def send_message(vector_db : MilvusClient =Depends(get_milvus_client)): 
+async def send_message(file: UploadFile, vector_db : MilvusClient =Depends(get_milvus_client)): 
+    """Feed raw documents to store into the vector database. This contains the whole indexing process of the RAG system"""
+    file_obj = file.file
+    if file_obj is None:
+        raise HTTPException("The file is invalid")
+    print(file_obj.read())
     return vector_db.list_collections(), vector_db.list_databases()
+    
