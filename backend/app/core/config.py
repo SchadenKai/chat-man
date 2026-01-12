23bef02c9f5b573f18b001b281aa0a02b3a1801b
SchadenKai <kairustecsonpersonal@gmail.com>
2026-01-12 20:55:47 +0800

    feat: add configuration options for database and API keys in Settings class

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -12,6 +12,16 @@ class Settings(BaseSettings):
     nebius_api_key: Optional[str]
     brave_api_key: Optional[str]
     gemini_api_key: Optional[str]
+    vector_dimension: Optional[int] = 1536
+
+    # Change default value as needed through .env file
+    milvus_uri: Optional[str] = "http://windows-server:19530"
+    milvus_db_name: Optional[str] = "chat_man_db"
+    collection_name: Optional[str] = "recipe_rag"
+    postgres_host: Optional[str] = "http://windows-server:5432"
+    postgres_db_name: Optional[str] = "chat_man_db"
+    postgres_user: Optional[str] = "root"
+    postgres_password: Optional[str] = "password"
 
 
 settings = Settings()
