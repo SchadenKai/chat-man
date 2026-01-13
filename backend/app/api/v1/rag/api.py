ea1bbd426e4b73d83a0e8e6f7a073d0713ee3d9a
SchadenKai <kairustecsonpersonal@gmail.com>
2026-01-13 14:26:22 +0800

    refactor: clean up imports and improve code formatting in api.py

--- backend/app/api/v1/rag/api.py
+++ backend/app/api/v1/rag/api.py
@@ -1,35 +1,35 @@
-from fastapi import APIRouter, Depends, HTTPException, Request, UploadFile
+from fastapi import APIRouter, Depends, HTTPException, UploadFile
 from fastapi.responses import StreamingResponse
 
-from langchain_core.language_models import BaseChatModel
 from pymilvus import MilvusClient
-from app.agent.react_agent.graph import call_agent
-from ag_ui.encoder import EventEncoder
-from ag_ui.core import RunAgentInput
 
-from app.agent.factory import get_default_llm
-from client.milvus_client import get_milvus_client, sample_embedding
-from langchain_text_splitters import RecursiveCharacterTextSplitter, TokenTextSplitter, MarkdownHeaderTextSplitter, CharacterTextSplitter
+from client.milvus_client import get_milvus_client
+from langchain_text_splitters import (
+    RecursiveCharacterTextSplitter,
+    TokenTextSplitter,
+    MarkdownHeaderTextSplitter,
+)
 from langchain_core.documents import Document
 
 router = APIRouter(prefix="/rag")
 
+
 def token_based_chunker(text: str) -> list[str]:
     chunker = TokenTextSplitter.from_tiktoken_encoder(
-        encoding_name="o200k_base",
-        chunk_size=10,
-        chunk_overlap=5
+        encoding_name="o200k_base", chunk_size=10, chunk_overlap=5
     )
     return chunker.split_text(text)
+
 
 def markdown_chunker(text: str) -> list[Document]:
     headers_to_split_on = [
         ("#", "Topic"),
         ("##", "Sub Topic"),
-        ("###", "Sub Sub Topic")
+        ("###", "Sub Sub Topic"),
     ]
     chunker = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
     return chunker.split_text(text)
+
 
 def charac_doc_chunker(document: Document) -> list[Document]:
     chunk_size = 250
@@ -38,6 +38,7 @@ def charac_doc_chunker(document: Document) -> list[Document]:
         chunk_size=chunk_size, chunk_overlap=chunk_overlap
     )
     return chunker.split_documents(document)
+
 
 @router.post("/sample_chunker")
 async def send_message(
@@ -51,10 +52,8 @@ async def send_message(
 
     docs_chunks = markdown_chunker(content)
     docs_chunks = charac_doc_chunker(docs_chunks)
-    return {
-        "total_chunks" : len(docs_chunks),
-        "chunks" : docs_chunks
-    }
+    return {"total_chunks": len(docs_chunks), "chunks": docs_chunks}
+
 
 @router.post("/ingest")
 async def ingest_document(
@@ -66,6 +65,7 @@ async def ingest_document(
     content = content.decode()
 
     docs_chunks = markdown_chunker(content)
+
 
 @router.post("/create-database")
 async def create_database(
